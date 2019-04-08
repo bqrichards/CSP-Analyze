8aca04b85c00789b77e3402174ae79f81d5d7135
Brandon Richards <bqrichards@icloud.com>
2019-04-08 11:09:05 -0500

    Create database

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import logging
+import models
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
+''' Database '''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scouting.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+with app.app_context():
+	models.db.init_app(app)
+
+''' Web Routes '''
 @app.route('/')
 @app.route('/index')
 @app.route('/leaderboards')
@@ -14,4 +23,6 @@ def leaderboards():
 def teams():
 	return render_template('teams.html', title='Teams')
 
+
+models.db.create_all(app=app)
 app.run(host='0.0.0.0')
